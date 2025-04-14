01f655191273890f5b96703af4bacd4513e5f0bf
lukethelehman <lukelehman05@gmail.com>
2025-04-14 10:54:17 -0400

    started stash method

---
+++ Items.java
@@ -0,0 +1,15 @@
+public class Items {
+	public static final int POINTS = 0;
+	public static final int WOOD = 1;
+	public static final int STONE = 2;
+	public static final int FOOD = 3;
+	public static final int WATER = 4;
+	public static final int BARTERTOKEN = 5;
+	public static final int FIREPIT = 6;
+	public static final int SHELTER = 7;
+	public static final int CABIN = 8;
+	public static final int OUTPOST = 9;
+	public static final String[] itemStrings = {"Points", "Wood", "Stone", "Food", "Water", 
+												"Barter Tokens", "Fire Pits", "Shelters", 
+												"Cabins", "Outposts"}; 
+}
--- Player.java
+++ Player.java
@@ -3,6 +3,7 @@
 public class Player {
 	int[] inventory = new int[10];
 	String name;
+	java.util.Scanner input = new java.util.Scanner(System.in);
 
 	Player(String name){
 		this.name = name;
@@ -10,5 +11,64 @@ public class Player {
 
 	public static void main(String args[]) {
 		Player p = new Player("lucas");
-	}	
+		p.addResource(Items.WOOD);
+		p.addResource(Items.WATER);
+		p.printInventory();
+	}
+
+	public void printInventory(){
+		System.out.println(name + "'s Inventory");
+		System.out.println(Items.itemStrings[Items.POINTS] + ": " + inventory[Items.POINTS]);
+		for (int i = 1; i < inventory.length; i++){
+			if (inventory[i] != 0){
+				System.out.println(Items.itemStrings[i] + ": " + inventory[i]);
+			}
+		}	
+	}
+
+	public void addResource(int resource){
+		inventory[resource]++;
+	}
+
+	public void stashResource(){
+		boolean keepGoing = true;
+		while (keepGoing){
+			System.out.println("What item would you like to stash?");
+			System.out.println("0.) Wood");
+			System.out.println("1.) Stone");
+			System.out.println("2.) Food");
+			System.out.println("3.) Water");
+			System.out.print("Please Enter (0-3)): ");
+			int response = getInt();
+
+			if (response >= 0 && response <= 3){
+				if (inventory[response] != 0){
+					inventory[response] = inventory[response] - 1;
+					inventory[Items.POINTS] ++;
+					System.out.println("You stashed one " + Items.itemStrings[response]);
+					System.out.println("You earned one point");
+					keepGoing = false;
+				}
+				else if (inventory[response] == 0) {
+					System.out.println("ERROR: No item to stash");
+					}
+			}
+			else {
+				System.out.println("ERROR: Please enter an integer (0-3)");
+			}
+		}	
+	}
+
+	public int getInt(){
+		String response = input.nextLine();
+		boolean keepGoing = true;
+		while (keepGoing){
+			try {	
+				keepGoing = false;
+				return Integer.parseInt(response);
+			} catch(NumberFormatException e) {
+				System.out.println("ERROR: please enter an integer");
+			}
+		}
+	}
 }
